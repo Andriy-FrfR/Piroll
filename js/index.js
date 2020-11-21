02e29e2ab76b47d90f326b874c36ab55b4a2cb10
unknown <andriy.lukashewwich@gmail.com>
2020-11-21 22:35:33 +0200

    Added animation for percents in skills section, optimized the code

--- js/index.js
+++ js/index.js
@@ -5,24 +5,22 @@
 const toggle = document.querySelector('.toggle'),
     nav = document.querySelector('.nav');
 
-toggle.addEventListener('click', toggleMenu);
-
-function toggleMenu() {
+const toggleMenu = () => {
     toggle.classList.toggle('active');
     nav.classList.toggle('active');
-}
+};
 
-let navItemActive;
+toggle.addEventListener('click', toggleMenu);
 
-nav.onmousedown = function() {
+nav.onmousedown = () => {
     return false;
 };
 
 //Nav-item active
 
-nav.addEventListener('click', navItem);
+let navItemActive;
 
-function navItem(event) {
+const navItem = (event) => {
     let target = event.target.closest('.nav-item');
 
     if (!target) {
@@ -36,7 +34,9 @@ function navItem(event) {
     target.classList.add('active');
 
     navItemActive = target;
-}
+};
+
+nav.addEventListener('click', navItem);
 
 // Slider
 
@@ -49,28 +49,102 @@ let sliderActiveImage = document.querySelector('.slider-image.active');
     
 sliderWrapper.style.marginLeft = (-sliderActiveImage.dataset.order + 1) * sliderInner.offsetWidth + 'px';
 
-for (let img of sliderImages) {
-    img.addEventListener('click', sliderImageClick);
-}
-
-function sliderImageClick() {
-    const currentItemOrder = +this.dataset.order;
+const sliderImageClick = (event) => {
+    const currentItemOrder = +event.currentTarget.dataset.order;
     let difference = currentItemOrder - +sliderActiveImage.dataset.order;
 
     sliderWrapper.style.marginLeft = parseFloat(window.getComputedStyle(sliderWrapper).marginLeft) - difference * sliderInner.offsetWidth + 'px';
 
     sliderActiveImage.src = 'images/slider/slider-passive.png';
-    this.src = 'images/slider/slider-active.png';
+    event.currentTarget.src = 'images/slider/slider-active.png';
 
-    sliderActiveImage = this;
+    sliderActiveImage = event.currentTarget;
+};
+
+for (let img of sliderImages) {
+    img.addEventListener('click', sliderImageClick);
 }
 
 //Animations
 
+function animate({timing, draw, duration}) {
+
+    let start = performance.now();
+  
+    requestAnimationFrame(function animate(time) {
+
+      let timeFraction = (time - start) / duration;
+      if (timeFraction > 1) {
+          timeFraction = 1;
+      }
+  
+      let progress = timing(timeFraction); // Getting progress (From 0 to 1) based on a timing-function
+  
+      draw(progress); // Rendering animation based on progress
+  
+      if (timeFraction < 1) {
+        requestAnimationFrame(animate);
+      }
+    });
+}
+
+const skillsItems = document.querySelectorAll('.skills-item'),
+    skillsItemsWrapper = document.querySelector('.skills-items-wrapper');
+
+const skillsAnimate = () => {
+
+    // Define blue bar's width
+
+    if (skillsItemsWrapper.classList.contains('animate') && !skillsItemsWrapper.classList.contains('animated')) {
+        animate({
+            timing(timeFraction) {
+                return timeFraction;
+            },
+            draw(progress) {
+                for(let elem of skillsItems) {
+                    const blueBar = elem.querySelector('.percents-blue-bar'),
+                        greyBar = elem.querySelector('.percents-grey-bar'),
+                        percents = +elem.querySelector('.skills-percents').dataset.target;
+    
+                    blueBar.style.width = greyBar.offsetWidth / 100 * percents * progress + 'px';
+                }
+    
+                skillsItemsWrapper.classList.add('animated');
+            },
+            duration: 600,
+        });
+    }
+
+    // Animation of percents
+
+    const percents = document.querySelectorAll('.skills-percents');
+
+    percents.forEach((element) => {
+        const updateCount = () => {
+            const target = +element.dataset.target;
+            const percents = +element.innerHTML;
+
+            const inc = 1;
+
+            if (percents < target) {
+                element.innerHTML = Math.ceil(percents + inc);
+                setTimeout(updateCount, 20);
+            } else {
+                element.innerHTML = target;
+            }
+        };
+
+        updateCount();
+    });
+};
+
+
 const animItems = document.querySelectorAll('.anim-items');
 
+// Animations on scroll
+
 if (animItems.length > 0) {
-    function animOnScroll() {
+    const animOnScroll = () => {
         for (let i = 0; i < animItems.length; i++) {
             const animItem = animItems[i];
             const animItemHeight = animItem.offsetHeight;
@@ -84,6 +158,7 @@ if (animItems.length > 0) {
             if ((window.pageYOffset > animItemOffset - animItemPoint) && (window.pageYOffset < animItemOffset + animItemHeight)) {
                 setTimeout(() => {
                     animItem.classList.add('animate');
+                    skillsAnimate();
                 }, animItem.dataset.animDelay);
             } else {
                 if (animItem.classList.contains('unanimate')) {
@@ -91,7 +166,7 @@ if (animItems.length > 0) {
                 }
             }
         }
-    }
+    };
 
     const topOffset = (elem) => {
         const elemRect = elem.getBoundingClientRect(),
@@ -103,52 +178,3 @@ if (animItems.length > 0) {
     animOnScroll();
     window.addEventListener('scroll', animOnScroll);
 }
-
-// Skills-bars animation
-
-function animate({timing, draw, duration}) {
-
-    let start = performance.now();
-  
-    requestAnimationFrame(function animate(time) {
-      // timeFraction изменяется от 0 до 1
-      let timeFraction = (time - start) / duration;
-      if (timeFraction > 1) {
-          timeFraction = 1;
-      }
-  
-      // вычисление текущего состояния анимации
-      let progress = timing(timeFraction);
-  
-      draw(progress); // отрисовать её
-  
-      if (timeFraction < 1) {
-        requestAnimationFrame(animate);
-      }
-    });
-}
-
-const skillsItems = document.querySelectorAll('.skills-item'),
-    skillsItemsWrapper = document.querySelector('.skills-items-wrapper');
-
-window.addEventListener('scroll', () => {
-    if (skillsItemsWrapper.classList.contains('animate') && !skillsItemsWrapper.classList.contains('animated')) {
-        animate({
-            timing(timeFraction) {
-                return timeFraction;
-            },
-            draw(progress) {
-                for(let elem of skillsItems) {
-                    const blueBar = elem.querySelector('.percents-blue-bar'),
-                        greyBar = elem.querySelector('.percents-grey-bar'),
-                        percents = +elem.querySelector('.skills-percents').innerHTML;
-
-                    blueBar.style.width = greyBar.offsetWidth / 100 * percents * progress + 'px';
-                }
-
-                skillsItemsWrapper.classList.add('animated')
-            },
-            duration: 300,
-        });
-    }
-});
